235706d4eed2db1bb378eff47f63a4cbbc67a54f
dongwook <boogiewooki02@gmail.com>
2026-02-10 15:33:14 +0900

    feat: S3 설정

---
+++ backend-spring/src/main/java/com/aidetector/global/util/S3Service.java
@@ -0,0 +1,38 @@
+package com.aidetector.global.util;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile multipartFile) throws IOException {
+        // 파일 이름 설정
+        String fileName = UUID.randomUUID() + "_" + multipartFile.getOriginalFilename();
+
+        // 메타 데이터 설정 (파일 크기 등)
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentLength(multipartFile.getSize());
+        objMeta.setContentType(multipartFile.getContentType());
+
+        // S3로 업로드
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, multipartFile.getInputStream(), objMeta));
+
+        // 업로드된 파일의 public url 반환
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+}
