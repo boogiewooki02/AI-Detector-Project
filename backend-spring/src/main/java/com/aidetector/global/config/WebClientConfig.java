4cab0a52b139a118f74a1b3fba25d1e8fc58a5e5
dongwook <boogiewooki02@gmail.com>
2026-02-12 17:37:12 +0900

    chore: fastapi url 수정

--- backend-spring/src/main/java/com/aidetector/global/config/WebClientConfig.java
+++ backend-spring/src/main/java/com/aidetector/global/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.aidetector.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${AI_SERVER_URL:http://localhost:8000}") // 환경 변수 읽기 (기본값 localhost)
+    private String aiServerUrl;
+
     @Bean
     public WebClient fastapiClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8000")
+                .baseUrl(aiServerUrl) // 변수 사용
                 .build();
     }
 }
